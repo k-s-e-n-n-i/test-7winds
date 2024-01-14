c9acdfa3bc09980461dd754a7d8b0f0dc4dbcb36
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2024-01-14 15:55:43 +0700

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { connect, WithStore, MapStateToProps, MapDispatchToProps } from "../../redux/services/Imports";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { connect, WithStore, MapStateToProps, MapDispatchToProps } from '../../redux/services/Imports';
 
-import "./App.scss";
-import Page404 from "../../pages/404/404";
+import './App.scss';
+import Page404 from '../../pages/404/404';
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const App = () => {
   const theme = createTheme({
     palette: {
-      mode: "dark",
+      mode: 'dark',
     },
   });
 
